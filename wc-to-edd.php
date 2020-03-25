7a131b50b99fc2b01e249efd7b66060e96ccefeb
Greggory Hogan <gregg@interactology.com>
2020-03-24 20:48:45 -0400

    Removing unneccessary files from previous iteration

--- wc-to-edd.php
+++ wc-to-edd.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce to EDD CSV Generator
 Description: This plugin provides connversion functionality from WooCommerce to Easy Digital Downloads
 Author: Greggory Hogan 
-Version: 0.1
+Version: 1.0
 Author URI: https://mynameisgregg.com
 */
 
@@ -11,17 +11,12 @@ Author URI: https://mynameisgregg.com
 function eos_admin_css() {
     wp_register_style( 'wc-edd-admin-css', plugins_url().'/wc-to-edd/css/admin.css' );
     wp_enqueue_style( 'wc-edd-admin-css' );
-    //wp_enqueue_script( 'eos-scripts', plugins_url() . '/eos-mentor/js/app.js', array('jquery'), null ,true ); 
-    //wp_localize_script('eos-scripts', 'settings', array('ajaxurl' => admin_url('admin-ajax.php')));
 }
 add_action('admin_init', 'eos_admin_css');
 
 /* Plugin Settings */
 include( plugin_dir_path( __FILE__ ) . 'inc/plugin_settings.php');
 
-/* Plugin Settings */
+/* Plugin Functions */
 include( plugin_dir_path( __FILE__ ) . 'inc/functions.php');
-
-/* AJAX Functions */
-include( plugin_dir_path( __FILE__ ) . 'inc/ajax.php');
 ?>
